05bf27b720d6431041d27db2aa1face9a68acce8
Andrey Kraynov <stonedrey@yahoo.com>
2017-08-03 11:10:30 +0300

    tge

--- tiegy/app/js/common.js
+++ tiegy/app/js/common.js
@@ -2,8 +2,29 @@ $(document).scroll(function(){
 	ofst = $(window).scrollTop();
 	if (ofst>0) {
 		$('.header').addClass('header_scrolled');
+		$('.logo-holder__image').addClass('logo-holder__image_small');
 	}
  else{
  	$('.header').removeClass('header_scrolled');
+ 	$('.logo-holder__image').removeClass('logo-holder__image_small');
  }
+});
+
+$('#header__burger').click(function(){
+	if ($('#nav').hasClass('navigation_mobile-active')) {
+		$('#nav').removeClass('navigation_mobile-active');
+	}
+	else{
+		$('#nav').addClass('navigation_mobile-active');
+	}
+
+});
+
+$('#mobile_nav_close').click(function(){
+if ($('#nav').hasClass('navigation_mobile-active')) {
+		$('#nav').removeClass('navigation_mobile-active');
+	}
+	else{
+		$('#nav').addClass('navigation_mobile-active');
+	}
 });
