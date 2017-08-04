ea5427e639178bf8a966b7a70ececddc05853f77
Andrey Kraynov <stonedrey@yahoo.com>
2017-08-04 14:33:02 +0300

    slider

--- tiegy/app/js/common.js
+++ tiegy/app/js/common.js
@@ -28,3 +28,21 @@ if ($('#nav').hasClass('navigation_mobile-active')) {
 		$('#nav').addClass('navigation_mobile-active');
 	}
 });
+
+//questions
+$('.faq-list__question').click(function(){
+	$('.faq-list__answer').slideUp();
+	$('.fa-minus').removeClass('fa-minus').addClass('fa-plus');
+	$(this).children('.fa-plus').removeClass('fa-plus').addClass('fa-minus');
+	$(this).siblings('.faq-list__answer').slideDown(400);
+	$(this).siblings('.faq-list__answer').css({'display':'flex'});
+});
+
+//owl-carousel
+  $(".owl-carousel").owlCarousel({
+  	items: 1,
+  	autoplay: true,
+  	autoplayTimeout: 8000,
+  	loop: true,
+  	autoplaySpeed: 1000
+  });
