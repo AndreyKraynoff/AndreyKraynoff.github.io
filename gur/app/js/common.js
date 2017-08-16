40fdb90664cc5ef22e60fdb2122de737ce92b679
Andrey Kraynov <stonedrey@yahoo.com>
2017-08-16 11:01:48 +0300

    betta main gur

--- gur/app/js/common.js
+++ gur/app/js/common.js
@@ -3,9 +3,28 @@ $(document).ready(function() {
     	delay: 200,
     	speed: "fast",
     	cssArrows: false
-    });
+    }); 
+
+function slides_move(){
+	 number = $('.slide_active').index('.slide');
+	 count = $('.slide').size();
+	 $('.slide_active').removeClass('slide_active');
+    if ((number+1) == count) {
+	$('.slide').eq(0).addClass('slide_active');
+	var rrr =setTimeout(slides_move, 8000);
+   }
+	else{
+		$('.slide').eq(number+1).addClass('slide_active');
+	var bbb =	setTimeout(slides_move, 8000);
+	}
+    
+}
+setTimeout(slides_move, 6000);
+
+
 
 $('.next_slide').click(function(){
+
 $(this).parent().removeClass('slide_active');
 number = $(this).parent().index('.slide');
 count = $('.slide').size();
@@ -31,22 +50,17 @@ if (number == 0) {
 });
 
 
-function slides_move(){
-	 number = $('.slide_active').index('.slide');
-	 count = $('.slide').size();
-	 $('.slide_active').removeClass('slide_active');
-    if ((number+1) == count) {
-	$('.slide').eq(0).addClass('slide_active');
-	setTimeout(slides_move, 9000);
-   }
-	else{
-		$('.slide').eq(number+1).addClass('slide_active');
-		setTimeout(slides_move, 9000);
-	}
-    
-}
-setTimeout(slides_move, 9000);
 
+
+
+
+/*$('.navigation').children('.navigation__item').hover(function(){
+	$(this).children('.sub-menu').slideDown(400);
+},function(){
+	$(this).children('.sub-menu').slideUp(400);
+});
+ function()
+*/
 
 
 });
