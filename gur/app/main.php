261be14f7a18f3f8870e46f0a7ad226207b7bef1
Andrey Kraynov <stonedrey@yahoo.com>
2017-08-16 12:07:37 +0300

    header black

---
+++ gur/app/main.php
@@ -0,0 +1,64 @@
+<section class="statements">
+        <div class="slide slide_active">
+			<img class="slide__img" src="img/velik.jpg">
+			<div class="slide__text">
+				<h1 class="slider__title">Lorem</h1>
+				<p class="slider__descr">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+			</div>
+			<div class="slide__imgmask"></div>	
+			<div class="arrow-holder-left prev_slide">
+                <i class="ion-ios-arrow-left"></i>
+			</div>	
+			<div  class="arrow-holder-right next_slide">
+                <i class="ion-ios-arrow-right"></i>
+			</div>
+		</div>
+
+		
+
+		<div class="slide">
+			<img class="slide__img" src="img/family.jpg">
+			<div class="slide__text">
+				<h1 class="slider__title">Шикарные семейные фото</h1>
+				<p class="slider__descr">Херни не делаю</p>
+			</div>
+			<div class="slide__imgmask"></div>	
+			<div class="arrow-holder-left prev_slide">
+                <i class="ion-ios-arrow-left"></i>
+			</div>	
+			<div  class="arrow-holder-right next_slide">
+                <i class="ion-ios-arrow-right"></i>
+			</div>
+		</div>
+		<div class="slide">
+			<img class="slide__img" src="img/ass.jpg">
+			<div class="slide__text">
+				<h1 class="slider__title">Фоткаю дам не только голыми</h1>
+				<p class="slider__descr">И не только в своей комнате</p>
+			</div>
+			<div class="slide__imgmask"></div>	
+			<div  class="arrow-holder-left prev_slide">
+                <i class="ion-ios-arrow-left"></i>
+			</div>	
+			<div class="arrow-holder-right next_slide">
+                <i class="ion-ios-arrow-right"></i>
+			</div>
+		</div>
+		<div class="slide">
+			<img class="slide__img" src="img/vice.jpg">
+			<div class="slide__text">
+				<h1 class="slider__title">ну и голыми в комнате конечно</h1>
+				<p class="slider__descr">отправь жену ко мне, при предъявлении свидетельства о браке скидон</p>
+			</div>
+			<div class="slide__imgmask"></div>	
+			<div class="arrow-holder-left prev_slide">
+                <i class="ion-ios-arrow-left"></i>
+			</div>	
+			<div  class="arrow-holder-right next_slide">
+                <i class="ion-ios-arrow-right"></i>
+			</div>
+		</div>
+
+		
+	
+	</section>
