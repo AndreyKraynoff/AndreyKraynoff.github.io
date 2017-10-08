aae850867175a1082b1dfce94fd4534db1b7143d
Andrey Kraynov <stonedrey@yahoo.com>
2017-10-08 13:53:29 +0300

    f

--- evolevel/app/js/common.js
+++ evolevel/app/js/common.js
@@ -10,7 +10,7 @@ $('.composition-text-smm').css({'min-height': +comp+'px'});
 
 //$('body').mCustomScrollbar({theme:"dark",mouseWheelPixels: 150,scrollInertia: 300});
 
-$(".phone_write_us").mask("+7(999) 999-9999");
+$(".phone_write_us").mask("+7(999)999-9999");
 
 
 
